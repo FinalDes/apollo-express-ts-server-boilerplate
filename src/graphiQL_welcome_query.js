9565b5a358c162a04995eeb0c35cc681a1eb39ff
Alex Alonso <alex4551@gmail.com>
2017-07-18 18:38:10 +0800

    add graphql script when open graphiql page

---
+++ src/graphiQL_welcome_query.js
@@ -0,0 +1,6 @@
+export const welcomeQuery = `# Welcome to GraphiQL
+
+query{
+  hi
+}
+`
--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import {schema} from './schema';
+import {welcomeQuery} from './graphiQL_welcome_query'
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT;
-var app = express();
+const app = express();
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql'
+  endpointURL: '/graphql',
+  query: welcomeQuery
 }));
 
 app.listen(PORT, () => {
