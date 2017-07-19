2093c0f111ceab9de051016f2e649e5fb249b696
Alex Alonso <alex4551@gmail.com>
2017-07-19 13:22:29 +0800

    add jest and nodemon

--- src/server.ts
+++ src/server.ts
@@ -23,3 +23,5 @@ app.listen(PORT, () => {
   console.log(`GraphQL Server is now running on http://localhost:${PORT}/graphql`);
   console.log(`View GraphiQL at http://localhost:${PORT}/graphiql`);
 });
+
+export default app;
---
+++ test/demo.test.ts
@@ -0,0 +1,5 @@
+describe("first test", () => {
+  test("first Test", () => {
+    expect(true).toBe(true);
+  });
+});
