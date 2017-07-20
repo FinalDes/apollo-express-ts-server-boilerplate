7289bea609ecd850411aa7f1fd3171ac9ee740c9
Alex Alonso <alex4551@gmail.com>
2017-07-20 14:19:05 +0800

    add helmet middleware and update jest config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express = require("express");
 import bodyParser = require("body-parser");
+import helmet = require("helmet");
 import { graphiqlExpress, graphqlExpress } from "graphql-server-express";
 
 import {welcomeQuery} from "./graphiQL_welcome_query";
 import {schema} from "./schema";
 
 const app = express();
+app.use(helmet());
 app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 app.use("/graphiql", graphiqlExpress({
   endpointURL: "/graphql",
