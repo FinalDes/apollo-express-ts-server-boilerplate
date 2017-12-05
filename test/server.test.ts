e25878ab2d2811ec6004088b10dd7eae2051e4a0
Alex Alonso <alex4551@gmail.com>
2017-12-05 16:22:18 +0800

    add missing type in test script

--- test/server.test.ts
+++ test/server.test.ts
@@ -5,42 +5,42 @@ import request = require("supertest");
 import app from "./../src/app";
 // should();
 
-const result = {data: {hi: "Hello World"}};
+const result: {data: {hi: string; }; } = {data: {hi: "Hello World"}};
 
 describe("Graphql test", () => {
-  it("Get Hi, content-type application/json", (done) => {
+  it("Get Hi, content-type application/json", (done: jest.DoneCallback) => {
     request(app)
     .post("/graphql")
     .set("Content-Type", "application/json; charset=UTF-8")
     .send({query: "{hi}"})
     .expect(200)
-    .end((err, res) => {
+    .end((err, res: request.Response) => {
       expect(err).toBeNull();
       expect(res.body).toEqual(result);
       // res.body.should.to.deep.equal(result);
       done();
     });
   });
-  it("Get Hi, content-type application/graphql", (done) => {
+  it("Get Hi, content-type application/graphql", (done: jest.DoneCallback) => {
     request(app)
     .post("/graphql")
     .set("Content-Type", "application/graphql; charset=UTF-8")
     .send("{hi}")
     .expect(200)
-    .end((err, res) => {
+    .end((err, res: request.Response) => {
       expect(err).toBeNull();
       expect(res.body).toEqual(result);
       // res.body.should.to.deep.equal(result);
       done();
     });
   });
-  it("Check headers", (done) => {
+  it("Check headers", (done: jest.DoneCallback) => {
     request(app)
       .post("/graphql")
       .set("Content-Type", "application/graphql; charset=UTF-8")
       .send("{hi}")
       .expect(200)
-      .end((err, res) => {
+      .end((err, res: request.Response) => {
         expect(err).toBeNull();
         expect(res.header["x-powered-by"]).toBeUndefined();
         done();
