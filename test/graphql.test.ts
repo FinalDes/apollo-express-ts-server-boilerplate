6e40928aa1a2f1ee2cf44a9c1c8c65f349424dae
Alex Alonso <alex4551@gmail.com>
2017-12-05 17:19:37 +0800

    update graphql test identation

--- test/graphql.test.ts
+++ test/graphql.test.ts
@@ -8,15 +8,15 @@ const result: {data: {hi: string; }; } = {data: {hi: "Hello World"}};
 describe("Graphql test", () => {
   it("Get Hi, content-type application/json", (done: jest.DoneCallback) => {
     request(app)
-    .post("/graphql")
-    .set("Content-Type", "application/json; charset=UTF-8")
-    .send({query: "{hi}"})
-    .expect(200)
-    .end((err, res: request.Response) => {
-      expect(err).toBeNull();
-      expect(res.body).toEqual(result);
-      // res.body.should.to.deep.equal(result);
-      done();
+      .post("/graphql")
+      .set("Content-Type", "application/json; charset=UTF-8")
+      .send({query: "{hi}"})
+      .expect(200)
+      .end((err, res: request.Response) => {
+        expect(err).toBeNull();
+        expect(res.body).toEqual(result);
+        // res.body.should.to.deep.equal(result);
+        done();
     });
   });
   it("Get Hi, content-type application/graphql", (done: jest.DoneCallback) => {
